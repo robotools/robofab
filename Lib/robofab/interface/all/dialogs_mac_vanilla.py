600e039b4ffb4065c47be46ce77730f49e81179d
Erik van Blokland <erik@letterror.com>
2011-10-21 15:00:37 +0000

    Plenty more dialogs for mac / vanilla applications. For instance SelectFont and SelectGlyph. Thanks to Frederik Berlaen.
    git-svn-id: http://svn.robofab.com/trunk@463 b5fa9d6c-a76f-4ffd-b3cb-f825fc41095c

--- Lib/robofab/interface/all/dialogs_mac_vanilla.py
+++ Lib/robofab/interface/all/dialogs_mac_vanilla.py
@@ -4,60 +4,188 @@
 
 """
 
+import vanilla
+from AppKit import NSApp, NSModalPanelWindowLevel, NSWindowCloseButton, NSWindowZoomButton, NSWindowMiniaturizeButton
+
 __all__ = [
-#    "AskString",
+    "AskString",
     "AskYesNoCancel",
-#    "FindGlyph",
-#    "GetFile",
-#    "GetFolder",
+    "FindGlyph",
+    "GetFile",
+    "GetFolder",
     "Message",
-#    "OneList",
+    "OneList",
     "PutFile",
-#    "SearchList",
-#    "SelectFont",
-#    "SelectGlyph",
+    "SearchList",
+    "SelectFont",
+    "SelectGlyph",
 #    "TwoChecks",
 #    "TwoFields",
 #    "ProgressBar",
 ]
 
+class _ModalWindow(vanilla.Window):
+    
+   nsWindowLevel = NSModalPanelWindowLevel 
+    
+   def __init__(self, *args, **kwargs):
+       super(_ModalWindow, self).__init__(*args, **kwargs)
+       self._window.standardWindowButton_(NSWindowCloseButton).setHidden_(True)        
+       self._window.standardWindowButton_(NSWindowZoomButton).setHidden_(True)
+       self._window.standardWindowButton_(NSWindowMiniaturizeButton).setHidden_(True)
+    
+   def open(self):
+       super(_ModalWindow, self).open()
+       self.center()
+       NSApp().runModalForWindow_(self._window)
+        
+   def windowWillClose_(self, notification):
+       super(_ModalWindow, self).windowWillClose_(notification)
+       NSApp().stopModal()
+
+class _baseWindowController(object):
+    
+    def setUpBaseWindowBehavior(self):
+        self._getValue = None
+        
+        self.w.okButton = vanilla.Button((-70, -30, -15, 20), "OK", callback=self.okCallback, sizeStyle="small")
+        self.w.setDefaultButton(self.w.okButton)
+
+        self.w.closeButton = vanilla.Button((-150, -30, -80, 20), "Cancel", callback=self.closeCallback, sizeStyle="small")
+        self.w.closeButton.bind(".", ["command"])
+        self.w.closeButton.bind(unichr(27), [])
+    
+    def okCallback(self, sender):
+        self.w.close()
+
+    def closeCallback(self, sender):
+        self.w.close()
+    
+    def get(self):
+        raise NotImplementedError
+        
+class _AskStringController(_baseWindowController):
+
+    def __init__(self, prompt, value, title):
+        self.w = _ModalWindow((370, 110), title)
+
+        self.w.infoText = vanilla.TextBox((15, 10, -15, 22), prompt)
+        self.w.input = vanilla.EditText((15, 40, -15, 22))
+        self.w.input.set(value)
+
+        self.setUpBaseWindowBehavior()
+        self.w.open()
+    
+    def get(self):
+        return self.w.input.get()
+    
+
+class _listController(_baseWindowController):
+    
+    def __init__(self, items, message, title, showSearch=False):
+        
+        self.items = items 
+        
+        self.w = _ModalWindow((250, 300), title)
+        y = 10
+        self.w.infoText = vanilla.TextBox((15, y, -15, 22), message)
+        y += 25
+        if showSearch:
+            self.w.search = vanilla.SearchBox((15, y, -15, 22), callback=self.searchCallback)
+            y += 25
+        self.w.itemList = vanilla.List((15, y, -15, -40), self.items, allowsMultipleSelection=False)
+        
+        self.setUpBaseWindowBehavior()
+        self.w.open()
+        
+    def searchCallback(self, sender):
+        search = sender.get()
+        
+        newItems = [item for item in self.items if repr(item).startswith(search)]
+        self.w.itemList.set(newItems)
+        if newItems:
+            self.w.itemList.setSelection([0])
+        
+    def get(self):
+        index = self.w.itemList.getSelection()
+        if index:
+            index = index[0]
+            return self.w.itemList[index]
+        return None
+
+    
 
 # start with all the defaults. 
 
 def AskString(prompt, value='', title='RoboFab'):
-    raise NotImplementedError
-
+    w = _AskStringController(prompt, value, title)
+    return w.get()
+    
 def AskYesNoCancel(prompt, title='RoboFab', default=0, informativeText=""):
-    import vanilla.dialogs
-    vanilla.dialogs.askYesNoCancel(messageText=prompt, informativeText=additionalText, resultCallback=_resultCallback)
+    return vanilla.dialogs.askYesNoCancel(messageText=prompt, informativeText=informativeText)
 
 def FindGlyph(aFont, message="Search for a glyph:", title='RoboFab'):
-    raise NotImplementedError
+    items = aFont.keys()
+    items.sort()
+    w = _listController(items, message, title, showSearch=True)
+    glyphName = w.get()
+    if glyphName is not None:
+        return aFont[glyphName]
+    return None
 
-def GetFile(message=None):
-    raise NotImplementedError
+def GetFile(message=None, allowsMultipleSelection=False):
+    result = vanilla.dialogs.getFile(messageText=message, allowsMultipleSelection=allowsMultipleSelection)
+    if result is None:
+        return None
+    if not allowsMultipleSelection:
+        return str(list(result)[0])
+    else:
+        return [str(n) for n in list(result)]
 
-def GetFolder(message=None):
-    raise NotImplementedError
+def GetFolder(message=None, allowsMultipleSelection=False):
+    result = vanilla.dialogs.getFolder(messageText=message, allowsMultipleSelection=allowsMultipleSelection)
+    if result is None:
+        return None
+    if not allowsMultipleSelection:
+        return str(list(result)[0])
+    else:
+        return [str(n) for n in list(result)]
 
 def Message(message, title='RoboFab', informativeText=""):
-    import vanilla.dialogs
     vanilla.dialogs.message(messageText=message, informativeText=informativeText)
     
 def OneList(list, message="Select an item:", title='RoboFab'):
     raise NotImplementedError
     
 def PutFile(message=None, defaultName=None):
-    raise NotImplementedError
+    return vanilla.dialogs.putFile(messageText=message, fileName=defaultName)
 
-def SearchList(list, message="Select an item:", title='RoboFab'):
-    raise NotImplementedError
+def SearchList(list, message="Select an item:", title='RoboFab'):    
+    w = _listController(list, message, title, showSearch=True)
+    return w.get()
 
 def SelectFont(message="Select a font:", title='RoboFab'):
-    raise NotImplementedError
+    from robofab.world import AllFonts
+    fonts = AllFonts()
+    
+    data = dict()
+    for font in fonts:
+        data["%s" %font] = font
+    
+    items = data.keys()
+    items.sort()
+    w = _listController(items, message, title, showSearch=False)
+    value = w.get()
+    return data[value]
 
-def SelectGlyph(font, message="Select a glyph:", title='RoboFab'):
-    raise NotImplementedError
+def SelectGlyph(aFont, message="Select a glyph:", title='RoboFab'):
+    items = aFont.keys()
+    items.sort()
+    w = _listController(items, message, title, showSearch=False)
+    glyphName = w.get()
+    if glyphName is not None:
+        return aFont[glyphName]
+    return None
 
 def TwoChecks(title_1="One",  title_2="Two", value1=1, value2=1, title='RoboFab'):
     raise NotImplementedError

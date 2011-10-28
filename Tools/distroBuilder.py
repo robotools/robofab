cc76ca5eb1d9c041782b8ae0ecd2b85550036942
Erik van Blokland <erik@letterror.com>
2011-10-28 21:46:41 +0000

    Distro building scripts. buildZip makes a zip of RoboFab, FontTools, DialogKit and Vabilla. buildZip_RobofabOnly makes a zip of (funny enough) only RoboFab. distroBuilder contains the moving parts. robofabDistroPaths contains a number of hardwired paths to the various trunks. Currently these are set up for Erik, but should you ever need to do this yourself, you need to update these paths before running. Note that these zips are not installers. They're just folders with conveniently packed code.
    git-svn-id: http://svn.robofab.com/trunk@484 b5fa9d6c-a76f-4ffd-b3cb-f825fc41095c

---
+++ Tools/distroBuilder.py
@@ -0,0 +1,139 @@
+"""
+
+	Attempt at a build script for an installer
+
+	1. copy stuff from the svn dirs to  here
+	2. get the version number
+	3. tell the installer to build
+	
+	
+	
+"""
+
+import os
+from robofabDistroPaths import *
+
+# copy the stuff from the svn dirs
+
+def updateFontTools():
+	pwd = os.getcwd()
+	os.chdir(fontToolsPath)
+	print os.system("svn up")
+	d = os.popen("svnversion")
+	revision = d.read()
+	os.chdir(pwd)
+	return revision.strip()
+
+def updateRobofab():
+	pwd = os.getcwd()
+	os.chdir(roboFabPath)
+	print os.system("svn up")
+	d = os.popen("svnversion")
+	revision = d.read()
+	os.chdir(pwd)
+	return revision.strip()
+
+def updateDialogKit():
+	pwd = os.getcwd()
+	os.chdir(roboFabPath)
+	print os.system("svn up")
+	d = os.popen("svnversion")
+	revision = d.read()
+	os.chdir(pwd)
+	return revision.strip()
+
+def copyStuffToBuildFolder(revision=None, distroRoot=None, only=None, makeZipName=None):
+	# robofab
+	
+	# make the target folder relative to this script
+	if distroRoot is None:
+		distroRoot = os.getcwd()
+	if not os.path.exists(distroRoot):
+		os.makedirs(distroRoot)
+	
+	if only is None:
+		only = ['fonttools', 'robofab', 'fonttools', 'dialogkit', 'vanilla']
+
+	if "scripts" in only:
+		srcDir = os.path.normpath(roboFabScriptsPath)
+		dstDir = os.path.normpath(os.path.join(distroRoot, "Scripts"))
+		cmd = 'rm -rf "%s"'%(dstDir)
+		os.popen(cmd)
+		copyStuff(srcDir, dstDir)
+	
+	if "robofab" in only:
+		srcDir = os.path.normpath(roboFabPath)
+		dstDir = os.path.normpath(os.path.join(distroRoot, "RoboFab"))
+		cmd = 'rm -rf "%s"'%(dstDir)
+		os.popen(cmd)
+		copyStuff(srcDir, dstDir)
+	
+	if "fonttools" in only:
+		# fontTools to the robofab distro build
+		srcDir = os.path.normpath(fontToolsPath)
+		dstDir = os.path.normpath(os.path.join(distroRoot, "FontTools"))
+		copyStuff(srcDir, dstDir)
+
+	if "dialogkit" in only:
+		# dialogKit to the robofab distro build
+		srcDir = os.path.normpath(dialogKitPath)
+		dstDir = os.path.normpath(os.path.join(distroRoot, "DialogKit"))
+		cmd = 'rm -rf "%s"'%(dstDir)
+		os.popen(cmd)
+		copyStuff(srcDir, dstDir)
+
+	if "vanilla" in only:
+		# vanilla to the robofab distro build
+		srcDir = os.path.normpath(dialogKitPath)
+		dstDir = os.path.normpath(os.path.join(distroRoot, "Vanilla"))
+		cmd = 'rm -rf "%s"'%(dstDir)
+		os.popen(cmd)
+		copyStuff(srcDir, dstDir)
+
+	if revision is not None:
+		notePath = os.path.join(roboFabPath, "svnRevision.txt")
+		print "notePath", notePath
+		f = open(notePath, 'w')
+		f.write(revision)
+		f.close()
+
+	if makeZipName:
+		os.chdir(distroRoot)
+		print "0000 distroRoot", distroRoot
+		archiveName = makeZipName%revision.strip()
+		#cmd = "zip -r %s RoboFab/"%archiveName
+		cmd = "zip -r \"%s\" *"%archiveName
+		print "--", cmd
+		d = os.popen(cmd)
+		print d.read()
+
+		cmd = "mv \"%s\" \"../\""%archiveName
+		print "--", cmd
+		d = os.popen(cmd)
+		print d.read()
+
+		cmd = "rm -rf \"%s\""%distroRoot
+		print "--", cmd
+		d = os.popen(cmd)
+		print d.read()
+
+
+def copyStuff(src, dst):
+	print "copyStuff src", src
+	print "copyStuff dst", dst
+	notCopy = ['.DS_Store', '.svn', ".pyc", "CVS", 'build', "build", "tags", "branches"]
+	for n in os.listdir(src):
+		if n in notCopy:
+			continue
+		if os.path.splitext(n)[-1] in notCopy:
+			continue
+		p = os.path.join(src, n)
+		d = os.path.join(dst, n)
+		if os.path.isdir(p):
+			if not os.path.exists(d):
+				os.makedirs(d)
+			copyStuff(p, d)
+			continue
+		cmd = 'cp "%s" "%s"'%(p, os.path.dirname(d))
+		print os.popen(cmd)
+
---
+++ Tools/robofabDistroPaths.py
@@ -0,0 +1,21 @@
+
+"""
+	Hard paths to packages which need to go into the installer / distribution.
+	If this is not running on Erik's machine, you need to change them. 
+
+"""
+
+# path to the robofab trunk
+roboFabPath = "/Users/erik/Develop/robofab/svn.robofab.com/trunk"
+
+# path to the Scripts folder in robofan
+roboFabScriptsPath = "/Users/erik/Develop/robofab/svn.robofab.com/trunk/Scripts"
+
+# path to the dialogKit trunk
+dialogKitPath = "/Users/erik/Develop/svn.typesupply.com/packages/dialogKit/trunk"
+
+# path to the vanilla trunk
+vanillaPath = "/Users/erik/Develop/svn.typesupply.com/packages/vanilla/trunk"
+
+# path to the fontTools trunk
+fontToolsPath = "/Users/erik/Develop/fonttools/svn.fonttools.com/trunk"

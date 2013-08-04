d777e64b19b91de6c65ea324223f8d576cf7896b
schriftgestalt <georg.seifert@mac.com>
2013-08-04 10:15:17 +0200

    reanable the undo call for FLS

--- Scripts/RoboFabIntro/demo_GlyphAppender.py
+++ Scripts/RoboFabIntro/demo_GlyphAppender.py
@@ -13,7 +13,10 @@ selected = SelectGlyph(font)
 # make sure that we are not trying add the current glyph to itself
 if selected.name != glyph.name:
 	# preserve the current state
-	#fl.SetUndo()
+	try:
+		fl.SetUndo()
+	except:
+		pass
 	# add the selected glyph to the current glyph
 	glyph.appendGlyph(selected)
 	# always update the glyph!

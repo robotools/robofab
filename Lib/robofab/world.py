9634f5577c7a3d40e1206c8bd12c3a38c3d16d70
Jens Kutilek <webmail@kutilek.de>
2016-03-03 09:59:00 +0100

    Don't try every app detection import, only try further if the previous one was unsuccessful.

--- Lib/robofab/world.py
+++ Lib/robofab/world.py
@@ -46,23 +46,23 @@ class RFWorld:
 			self.applicationName = bundle.infoDictionary()["CFBundleName"]
 			self.inGlyphs = True
 			self.glyphsVersion = bundle.infoDictionary()["CFBundleVersion"]
-		except ImportError: pass
-		# are we in RoboFont
-		try:
-			import mojo
-			from AppKit import NSBundle
-			bundle = NSBundle.mainBundle()
-			self.applicationName = bundle.infoDictionary()["CFBundleName"]
-			self.inRoboFont = True
-			self.roboFontVersion = bundle.infoDictionary()["CFBundleVersion"]
-		except ImportError: pass
-		# are we in FontLab?
-		try:
-			from FL import fl
-			self.applicationName = fl.filename
-			self.inFontLab = True
-			self.flVersion = fl.version
-		except ImportError: pass
+		except ImportError:
+			# are we in RoboFont
+			try:
+				import mojo
+				from AppKit import NSBundle
+				bundle = NSBundle.mainBundle()
+				self.applicationName = bundle.infoDictionary()["CFBundleName"]
+				self.inRoboFont = True
+				self.roboFontVersion = bundle.infoDictionary()["CFBundleVersion"]
+			except ImportError:
+				# are we in FontLab?
+				try:
+					from FL import fl
+					self.applicationName = fl.filename
+					self.inFontLab = True
+					self.flVersion = fl.version
+				except ImportError: pass
 		# we are in NoneLab
 		if not self.inFontLab:
 			self.inPython = True
